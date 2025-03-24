2d82f4b315b876a5349dec16de7e41446d4aba0f
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2025-03-24 18:25:23 +0100

    define CRUD routes for product, update, and update point

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import express from 'express'
+import router from './router'
+import morgan from 'morgan'
 
 const app = express()
+
+const customLogger = (message) => (req, res, next) => {
+  console.log(`Hello from ${message}`)
+  next()
+}
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(customLogger('custom logger'))
 
 app.get('/', (req, res) => {
     console.log('hello from express')
     res.status(200)
     res.json({message: 'hello'})
 })
+
+app.use('/api', router)
 
 export default app
